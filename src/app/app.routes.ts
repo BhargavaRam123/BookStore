b427298625df084ac85b341b12a9b07153f9d257
Bhargava Ram A <122313187+BhargavaRam123@users.noreply.github.com>
2025-06-17 11:54:02 +0530

    routes setted for signup

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { SignupComponent } from './components/pages/signup/signup.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'home', component: AppComponent },
+  { path: 'signin', component: SignupComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+];
