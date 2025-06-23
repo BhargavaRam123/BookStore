567f0a37db5d73451da30e4e8389ff8c6f1c8213
Bhargava Ram A <122313187+BhargavaRam123@users.noreply.github.com>
2025-06-23 17:24:15 +0530

    [bhargav]: feat: add logout functionality and enhance user profile menu

--- src/app/components/appbar/appbar.component.ts
+++ src/app/components/appbar/appbar.component.ts
@@ -3,16 +3,39 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatButtonModule } from '@angular/material/button';
 import { ModalService } from '../../services/commonservice/modal.service';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-appbar',
-  imports: [MatIconModule, MatMenuModule, MatButtonModule],
+  imports: [MatIconModule, MatMenuModule, MatButtonModule, CommonModule],
   templateUrl: './appbar.component.html',
   styleUrl: './appbar.component.css',
 })
 export class AppbarComponent {
-  constructor(private modalService: ModalService) {}
-
+  constructor(private modalService: ModalService, private router: Router) {}
+  toProfile() {
+    this.router.navigate(['/userprofile']);
+  }
+  toHome() {
+    this.router.navigate(['/home']);
+  }
+  toCart() {
+    this.router.navigate(['/cart']);
+  }
+  toWishList() {
+    this.router.navigate(['/wishlist']);
+  }
   openModal() {
     this.modalService.openModal();
+  }
+  getName() {
+    return localStorage.getItem('userFullName');
+  }
+  accessToken(): string | null {
+    return localStorage.getItem('accessToken');
+  }
+  onLogout() {
+    localStorage.setItem('accessToken', '');
+    this.router.navigate(['/home']);
   }
 }
